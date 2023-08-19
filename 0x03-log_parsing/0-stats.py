1e7a6a0e210176dac449c76c763a2045623d8e4d
Augustie A. Lartey <augustinelartey68@gmail.com>
2023-08-19 21:02:24 +0000

    Corrections

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,64 +1,88 @@
 #!/usr/bin/python3
-"""A short function for use with 0-generator.py """
-import sys
+'''A script for parsing HTTP request logs.
+'''
+import re
 
 
-def parse_data():
-    """
-    parse_data is a function that will read lines from stdin
-    which include file sizes and status codes from websites.
+def extract_input(input_line):
+    '''Extracts sections of a line of an HTTP request log.
+    '''
+    fp = (
+        r'\s*(?P<ip>\S+)\s*',
+        r'\s*\[(?P<date>\d+\-\d+\-\d+ \d+:\d+:\d+\.\d+)\]',
+        r'\s*"(?P<request>[^"]*)"\s*',
+        r'\s*(?P<status_code>\S+)',
+        r'\s*(?P<file_size>\d+)'
+    )
+    info = {
+        'status_code': 0,
+        'file_size': 0,
+    }
+    log_fmt = '{}\\-{}{}{}{}\\s*'.format(fp[0], fp[1], fp[2], fp[3], fp[4])
+    resp_match = re.fullmatch(log_fmt, input_line)
+    if resp_match is not None:
+        status_code = resp_match.group('status_code')
+        file_size = int(resp_match.group('file_size'))
+        info['status_code'] = status_code
+        info['file_size'] = file_size
+    return info
 
-    The function will sum up the file sizes into a total, and
-    every 10 lines read (or at the time of a keyboard interrupt)
-    the program will output a total size of the summed file sizes
-    and a tally of the status codes we received.
-    """
 
-    count = 0  # Tracks 10 iterations for printing
-    codes = []  # All valid status codes received
-    statuscodes = [200, 301, 400, 401, 403, 404, 405, 500]  # Valid codes
-    filesizes = []  # list of file sizes received
+def print_statistics(total_file_size, status_codes_stats):
+    '''Prints the accumulated statistics of the HTTP request log.
+    '''
+    print('File size: {:d}'.format(total_file_size), flush=True)
+    for status_code in sorted(status_codes_stats.keys()):
+        num = status_codes_stats.get(status_code, 0)
+        if num > 0:
+            print('{:s}: {:d}'.format(status_code, num), flush=True)
 
+
+def update_metrics(line, total_file_size, status_codes_stats):
+    '''Updates the metrics from a given HTTP request log.
+
+    Args:
+        line (str): The line of input from which to retrieve the metrics.
+
+    Returns:
+        int: The new total file size.
+    '''
+    line_info = extract_input(line)
+    status_code = line_info.get('status_code', '0')
+    if status_code in status_codes_stats.keys():
+        status_codes_stats[status_code] += 1
+    return total_file_size + line_info['file_size']
+
+
+def run():
+    '''Starts the log parser.
+    '''
+    line_num = 0
+    total_file_size = 0
+    status_codes_stats = {
+        '200': 0,
+        '301': 0,
+        '400': 0,
+        '401': 0,
+        '403': 0,
+        '404': 0,
+        '405': 0,
+        '500': 0,
+    }
     try:
-        for line in sys.stdin:  # Reading lines from stdin
-            if line == "":  # if empty line, skip
-                continue
-            count += 1
-            parsing = line.split()  # Break up line by spaces
-            # This is a hacky way to check format of stdin
-            if len(parsing) < 2:
-                continue
-            if not (parsing[-1].isnumeric()):
-                continue
-            if not (parsing[-2].isnumeric()):
-                continue
-            filesizes.append(int(parsing[-1]))  # last element is filesize
-            if int(parsing[-2]) in statuscodes:
-                # second to last is status code
-                codes.append(int(parsing[-2]))
-            if count % 10 == 0:  # Every ten rounds print data
-                # sumall file sizes
-                print("File size: {}".format(sum(filesizes)))
-                # dict of codes and count
-                codes_dict = {i: codes.count(i) for i in codes}
-                for k in sorted(codes_dict.keys()):
-                    print("{}: {}".format(k, codes_dict[k]))
-        # we leave the loop if we run out of lines
-        # If we didn't just print we need to
-        if count % 10 != 0:
-            print("File size: {}".format(sum(filesizes)))
-            codes_dict = {i: codes.count(i) for i in codes}
-            for k in sorted(codes_dict.keys()):
-                print("{}: {}".format(k, codes_dict[k]))
-        elif count == 0:
-            print("File size: {}".format(sum(filesizes)))
-    # If a ctrl C is sent, final print and quit
-    except KeyboardInterrupt:
-        print("File size: {}".format(sum(filesizes)))
-        codes_dict = {i: codes.count(i) for i in codes}
-        for k in sorted(codes_dict.keys()):
-            print("{}: {}".format(k, codes_dict[k]))
-        return
+        while True:
+            line = input()
+            total_file_size = update_metrics(
+                line,
+                total_file_size,
+                status_codes_stats,
+            )
+            line_num += 1
+            if line_num % 10 == 0:
+                print_statistics(total_file_size, status_codes_stats)
+    except (KeyboardInterrupt, EOFError):
+        print_statistics(total_file_size, status_codes_stats)
+
 
 if __name__ == '__main__':
-    parse_data()
+    run()
